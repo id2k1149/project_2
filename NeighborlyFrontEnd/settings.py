2fae31877b42f8aa0239b3508153c8f24458cf2f
id2k1149 <id2k1149@gmail.com>
2021-05-18 14:09:27 -0400

    settings

--- NeighborlyFrontEnd/settings.py
+++ NeighborlyFrontEnd/settings.py
@@ -1,6 +1,6 @@
 #--------- Flask settings
 # SERVER_HOST = '0.0.0.0' # Update this for the appropriate front-end website when up
-SERVER_HOST = 'https://myneighborlyapiv2.azurewebsites.net'
+SERVER_HOST = 'https://neighborlyfrontend.azurewebsites.net'
 SERVER_PORT = 5000
 # FLASK_DEBUG = True # Do not use debug mode in prod
 
