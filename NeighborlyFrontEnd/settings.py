f759ef62ef452e93b60f2cfb4cfa7f7bb5c1c3f3
id2k1149 <id2k1149@gmail.com>
2021-06-11 19:26:32 +0300

    settings

--- NeighborlyFrontEnd/settings.py
+++ NeighborlyFrontEnd/settings.py
@@ -1,6 +1,6 @@
 #--------- Flask settings
 # SERVER_HOST = '0.0.0.0' # Update this for the appropriate front-end website when up
-SERVER_HOST = 'https://neighborlyfrontend.azurewebsites.net'
+SERVER_HOST = 'https://myneighborlyapi1149.azurewebsites.net/api'
 SERVER_PORT = 5000
 # FLASK_DEBUG = True # Do not use debug mode in prod
 
@@ -12,9 +12,8 @@ RESTPLUS_404_HELP = True
 API_VERSION = 'v1'
 
 #-------- Azure constants
-
 # API_URL format: "https://[FUNCTION_APP_NAME_GOES_HERE].azurewebsites.net"
-API_URL =  "https://myneighborlyapiv2.azurewebsites.net/api"
+API_URL =  "https://myneighborlyapi1149.azurewebsites.net/api"
 
 # for local host if Azure functions served locally
 # API_URL = "http://localhost:7071/api"
